6b7aec49a02666aac41d456af72d60b6e61e8c23
Jayson Caburatan <montierdejayson@gmail.com>
2024-11-16 23:33:38 +0800

    Added Model Property and Make CRUD for the property

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -3,28 +3,30 @@ import prisma from "../lib/prisma.js";
 import jwt from "jsonwebtoken";
 
 export const register = async (req, res) => {
-  const { username, email, password } = req.body;
+  const { firstName, lastName, email, password } = req.body;
 
   try {
-    const existingUser = await prisma.user.findUnique({ where: { username } });
-    if (existingUser) {
+    const existingEmail = await prisma.user.findUnique({ where: { email } });
+
+    if (existingEmail) {
       return res.status(400).json({ message: "Username already exists" });
     }
 
-    if (!username || !email || !password) {
+    if (!firstName || !lastName || !email || !password) {
       return res.status(400).json({ message: "Fill out the require" });
     }
 
     const hashedPassword = await bcrypt.hash(password, 10);
     const newUser = await prisma.user.create({
       data: {
-        username,
-        email,
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
         password: hashedPassword,
       },
     });
 
-    res.status(201).json({ message: "User created successfully" });
+    res.status(201).json({ message: "User created successfully", newUser });
   } catch (error) {
     console.error("Error during registration:", error);
     res.status(500).json({ message: "Failed to create user" });
@@ -32,25 +34,19 @@ export const register = async (req, res) => {
 };
 
 export const login = async (req, res) => {
-  const { username, password } = req.body;
+  const { email, password } = req.body;
 
   try {
-    // CHECK IF THE USER EXISTS
-
     const user = await prisma.user.findUnique({
-      where: { username },
+      where: { email },
     });
 
     if (!user) return res.status(400).json({ message: "Invalid Credentials!" });
-
-    // CHECK IF THE PASSWORD IS CORRECT
 
     const isPasswordValid = await bcrypt.compare(password, user.password);
 
     if (!isPasswordValid)
       return res.status(400).json({ message: "Invalid Credentials!" });
-
-    // GENERATE COOKIE TOKEN AND SEND TO THE USER
 
     // res.setHeader("Set-Cookie", "test=" + "myValue").json("success")
     const age = 1000 * 60 * 60 * 24 * 7;
@@ -69,11 +65,11 @@ export const login = async (req, res) => {
     res
       .cookie("token", token, {
         httpOnly: true,
-        // secure:true,
+        secure: true,
         maxAge: age,
       })
       .status(200)
-      .json(userInfo);
+      .json({ token, userInfo });
   } catch (err) {
     console.log(err);
     res.status(500).json({ message: "Failed to login!" });
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,6 +1,20 @@
 import express from "express";
+
+import {
+  getUsers,
+  getUser,
+  updateUser,
+  deleteUser,
+  updatePassword,
+} from "../controllers/user.controller.js";
+import { verifyToken } from "../middleware/verifyToken.js";
 const router = express.Router();
 
-router.get("/test");
+router.get("/", getUsers);
+router.get("/:id", verifyToken, getUser);
+router.put("/:id", verifyToken, updateUser);
+router.put("/password/:id", verifyToken, updatePassword);
+
+router.delete("/:id", verifyToken, deleteUser);
 
 export default router;
